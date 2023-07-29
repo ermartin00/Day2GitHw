da0e2a41d05006e415aec23215e0a1dc19e0787f
ermartin00 <140658480+ermartin00@users.noreply.github.com>
2023-07-29 14:20:37 -0400

    Update Day2GitHwtest2.java

--- src/hwday2test/Day2GitHwtest2.java
+++ src/hwday2test/Day2GitHwtest2.java
@@ -5,4 +5,8 @@ public class Day2GitHwtest2 {
 	public static void main(String[] args) {
 		System.out.println("Today Wheater condition");
 	}
+	public static void getData() {
+		System.out.println("Method created in github ");
+	}
+	
 }
