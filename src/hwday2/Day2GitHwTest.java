c295a22406311659fad57e20ca9cd95374f41884
ermar <ermar@DESKTOP-0CSLR0O.attlocal.net>
2023-07-29 14:03:07 -0400

    create getStudentData() method

--- src/hwday2/Day2GitHwTest.java
+++ src/hwday2/Day2GitHwTest.java
@@ -2,4 +2,12 @@ package hwday2;
 
 public class Day2GitHwTest {
 
+	public static void main(String[] args) {
+		System.out.println("Homework day 2");
+	}
+	
+	public static void getStudentData() {
+		System.out.println("Name: John");
+		System.out.println("LastName: Cena");
+	}
 }
